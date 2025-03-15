ca3e4373e4befdb20490c459f7fca5a14d313f25
Chei <11056016@ntub.edu.tw>
2025-03-15 16:14:33 +0800

    0315

--- mysite/views.py
+++ mysite/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("hello world")
+
+def indexhome(request):
+    return render(request, 'indexhome.html')
